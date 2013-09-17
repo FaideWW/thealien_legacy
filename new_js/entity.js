a6d32b047c8c293a5a333ba50cc9290b9aeeb46a
faide <michael@faide.net>
2013-09-16 20:16:15 -0700

    More entity/scene refactoring

--- new_js/entity.js
+++ new_js/entity.js
@@ -1,19 +1,39 @@
 var alien = alien || {};
 
 alien.Entity = function(properties) {
+    //clone any properties into a new object
 	properties = properties || {};
-	var t = {};
 	for (var k in properties) {
 		if (properties.hasOwnProperty(k)) {
-			t[k] = properties[k];
+			this[k] = properties[k];
 		}
 	}
-	return t;
+    //if listeners have been cloned over, keep them
+    //otherwise start a new list
+    this.listeners = this.listeners || [];
 };
+
 alien.Entity.prototype.extend = function(extension) {
 	for (var k in extension) {
 		if (extension.hasOwnProperty(k)) {
 			this[k] = extension[k];
 		}
 	}
-}
+};
+
+alien.Entity.prototype.on = function(event, callback) {
+    this.listeners[event] = this.listeners[event] || [];
+    this.listeners[event].push(callback);
+};
+
+alien.Entity.prototype.trigger = function(event, data) {
+    if (this.listeners[event]) {
+        for (var k = 0; k < this.listeners[event].length; k++) {
+            this.listeners[event][i](this, data);
+        }
+    }
+};
+
+alien.Entity.prototype.update = function(dt) {
+    this.trigger('update', dt);
+};
--- new_js/scene.js
+++ new_js/scene.js
@@ -3,16 +3,14 @@ var alien = alien || {};
 alien.Scene = function(properties) {
     properties = properties || {};
     var t = {};
-
     t.entities = [];
-
     for (var k in properties) {
         if (properties.hasOwnProperty(k)) {
             t[k] = properties[k];
         }
     }
     return t;
-}
+};
 
 alien.Scene.prototype.extend = function(extension) {
     for (var k in extension) {
@@ -20,12 +18,12 @@ alien.Scene.prototype.extend = function(extension) {
             this[k] = extension[k];
         }
     }
-}
+};
 
 alien.Scene.prototype.addEntity = function(entity) {
     this.entities.push(entity);
     return (this.entities.length - 1);
-}
+};
 
 //this is a deceptively expensive operation (O(n)) for large scenes, 
 //so use it sparingly.  maybe rewrite later to accept entity indexing
@@ -36,49 +34,21 @@ alien.Scene.prototype.find = function(entity) {
         }
     }
     return -1;
-}
+};
 
 alien.Scene.prototype.removeEntity = function(entity) {
-    //if entity is a number
-        
-}
-
-alien.Scene = function() {
-    var none = {
-        wrap: false,
-        entities: []
-    };
-    var loaded = none;
-
-    Object.freeze(none);
-
-    return {
-        scenes: {
-            clone: function(scene) {
-                return {
-                    wrap: scene.wrap,
-                    entities: scene.entities
-                };
-            },
-            create: function(properties) {
-                //defaults
-                properties = properties || {};
-                properties.wrap = properties.wrap || false;
-                properties.entities = properties.entities || [];
-                return properties;
-            }
-        },
-        current: function() {
-            return loaded;
-        },
-        load: function(scene) {
-            this.unload();
-            loaded = scene;
-        },
-        unload: function() {
-            loaded = none;
-            return none;
+    if (typeof entity === 'number') {
+        //the entity is an index
+        if (entity === -1) {
+            //find() returned empty, i.e. the entity is not in the array
+            return null;
         }
-    };
+        return this.entities.splice(entity,1);
+    } else {
+        return this.removeEntity(this.find(entity));
+    }
+};
+
+alien.SceneManager = function() {
 
 }();
